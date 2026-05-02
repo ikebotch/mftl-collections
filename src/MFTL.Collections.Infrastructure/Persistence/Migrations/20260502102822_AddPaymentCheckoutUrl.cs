ffa1a1eadf1a9b05903ca19543ca41fbb282d9f2
ikebotch <ikebotch@gmail.com>
2026-05-02 11:55:22 +0100

    feat: add payment checkout url migration

---
+++ src/MFTL.Collections.Infrastructure/Persistence/Migrations/20260502102822_AddPaymentCheckoutUrl.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MFTL.Collections.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPaymentCheckoutUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CheckoutUrl",
+                table: "Payments",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CheckoutUrl",
+                table: "Payments");
+        }
+    }
+}
